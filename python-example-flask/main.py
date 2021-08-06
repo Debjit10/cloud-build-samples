50fbba45feaed5a852bc6ca9c2b2720cd7b9d027
Debjit Das <debjit.das@ingka.com>
2021-08-06 10:25:16 +0200

    Update

--- python-example-flask/main.py
+++ python-example-flask/main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "Universe")
+    name = os.environ.get("NAME", "Everyone!")
     return f"Hello {name}!"
 
 
